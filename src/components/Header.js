a255d0509b91d0721dbaf7722e6b6b2bafc36e9f
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-09-19 23:42:22 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,7 @@ function Header() {
         </div>
         
         
-        <div>
-          <input  className='search-box' type="text" placeholder="Search..." />
-          <img className='search-box pt-5'src='https://th.bing.com/th/id/R.34f3f3420bb165594ebd7a83dc769905?rik=IblMZKG7qdPC2g&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_588.png&ehk=AtPYPfnEvR%2fGMFfZ2RTWlKqLtTjS7CNEJmA3y8QMjyU%3d&risl=&pid=ImgRaw&r=0'></img>
-        </div>
+     
         
         <div>
           <h3>Cart <sup>{3}</sup></h3>
